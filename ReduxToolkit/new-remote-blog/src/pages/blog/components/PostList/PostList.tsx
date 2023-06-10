1468b8a38f90d02bcc4a83bc3619b56e36fba996
Khanh Sy <sykhanh1996@gmail.com>
2023-06-11 00:23:26 +0700

    setup rtk query

--- ReduxToolkit/new-remote-blog/src/pages/blog/components/PostList/PostList.tsx
+++ ReduxToolkit/new-remote-blog/src/pages/blog/components/PostList/PostList.tsx
@@ -1,37 +1,11 @@
-import { RootState, useAppDispatch } from 'store'
+import { useGetPostsQuery } from 'pages/blog/blog.service'
 import PostItem from '../PostItem'
-import { useSelector } from 'react-redux'
-import { deletePost, getPostList, startEditingPost } from 'pages/blog/blog.slice'
-import { Fragment, useEffect } from 'react'
+import { Fragment } from 'react'
 import SkeletonPost from '../SkeletonPost'
 
-// Gọi API trong useEffect()
-// Nếu gọi thành công thì dispatch action type: "blog/getPostListSuccess"
-// Nếu gọi thất bại thì dispatch action type: "blog/getPostListFailed"
-
-//
-
 export default function PostList() {
-  const postList = useSelector((state: RootState) => state.blog.postList)
-  const loading = useSelector((state: RootState) => state.blog.loading)
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    const promise = dispatch(getPostList())
-
-    return () => {
-      promise.abort()
-    }
-  }, [dispatch])
-
-  const handleDelete = (postId: string) => {
-    dispatch(deletePost(postId))
-  }
-  const handleStartEditing = (postId: string) => {
-    dispatch(startEditingPost(postId))
-  }
-
-  console.log(postList)
+  const { data, isLoading, isFetching } = useGetPostsQuery()
+  console.log(data, isLoading, isFetching)
   return (
     <div className='bg-white py-6 sm:py-8 lg:py-12'>
       <div className='mx-auto max-w-screen-xl px-4 md:px-8'>
@@ -42,15 +16,12 @@ export default function PostList() {
           </p>
         </div>
         <div className='grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-2 xl:grid-cols-2 xl:gap-8'>
-          {loading && (
+          {isFetching && (
             <Fragment>
               <SkeletonPost />
             </Fragment>
           )}
-          {!loading &&
-            postList.map((post) => (
-              <PostItem post={post} key={post.id} handleDelete={handleDelete} handleStartEditing={handleStartEditing} />
-            ))}
+          {!isFetching && data?.map((post) => <PostItem key={post.id} post={post} />)}
         </div>
       </div>
     </div>
--- ReduxToolkit/new-remote-blog/src/store.ts
+++ ReduxToolkit/new-remote-blog/src/store.ts
@@ -1,14 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/dist/query'
+import { blogApi } from 'pages/blog/blog.service'
 import blogReducer from 'pages/blog/blog.slice'
-import { useDispatch } from 'react-redux'
+// ...
 
 export const store = configureStore({
-  reducer: { blog: blogReducer }
+  reducer: {
+    blog: blogReducer,
+    [blogApi.reducerPath]: blogApi.reducer
+  },
+
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(blogApi.middleware)
 })
 
-// Lấy RootState và AppDispatch từ store của chúng ta
+setupListeners(store.dispatch)
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
-
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
-
-export const useAppDispatch = () => useDispatch<AppDispatch>()
