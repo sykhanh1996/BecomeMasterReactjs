a590d7aee1f28023047d2cc3aa439b8427944997
Khanh Sy <sykhanh1996@gmail.com>
2023-06-24 14:17:45 +0700

    UI for Register Layout

--- Clone-Shopee-Practice/ShopeeCloneK/src/layouts/RegisterLayout/RegisterLayout.tsx
+++ Clone-Shopee-Practice/ShopeeCloneK/src/layouts/RegisterLayout/RegisterLayout.tsx
@@ -1,3 +1,7 @@
+import { Outlet } from 'react-router-dom'
+import Footer from 'src/components/Footer'
+import RegisterHeader from 'src/components/RegisterHeader'
+
 interface Props {
   children?: React.ReactNode
 }
@@ -5,8 +9,10 @@ interface Props {
 export default function RegisterLayout({ children }: Props) {
   return (
     <div>
-      RegisterLayout
+      <RegisterHeader />
       {children}
+      <Outlet />
+      <Footer />
     </div>
   )
 }
