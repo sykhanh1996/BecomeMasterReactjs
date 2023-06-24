2e2ed770b53e52856b3bfcc51ef4d9134a6a1b2e
Khanh Sy <sykhanh1996@gmail.com>
2023-06-25 01:10:05 +0700

    config axios and react-query

--- Clone-Shopee-Practice/ShopeeCloneK/src/main.tsx
+++ Clone-Shopee-Practice/ShopeeCloneK/src/main.tsx
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
