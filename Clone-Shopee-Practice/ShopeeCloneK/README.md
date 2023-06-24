187b2031bdac3effcc095c37541c66ecb0e49a0e
Khanh Sy <sykhanh1996@gmail.com>
2023-06-24 16:04:28 +0700

    UI for Login

--- Clone-Shopee-Practice/ShopeeCloneK/README.md
+++ Clone-Shopee-Practice/ShopeeCloneK/README.md
@@ -1,1 +1,7 @@
 yarn add eslint prettier @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-config-prettier eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-prettier prettier-plugin-tailwindcss eslint-plugin-react-hooks -D
+
+yarn add -D @types/node
+
+yarn add react-router-dom
+
+yarn add react-hook-form
--- Clone-Shopee-Practice/ShopeeCloneK/src/pages/Login/Login.tsx
+++ Clone-Shopee-Practice/ShopeeCloneK/src/pages/Login/Login.tsx
@@ -1,3 +1,46 @@
+import { Link } from 'react-router-dom'
+import Button from 'src/components/Button'
+import Input from 'src/components/Input'
+
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <div className='bg-orange'>
+      <div>
+        <title>Đăng nhập | Shopee Clone</title>
+        <meta name='description' content='Đăng nhập vào dự án Shopee Clone' />
+      </div>
+      <div className='container'>
+        <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form className='rounded bg-white p-10 shadow-sm' noValidate>
+              <div className='text-2xl'>Đăng nhập</div>
+              <Input name='email' type='email' className='mt-8' placeholder='Email' />
+              <Input
+                name='password'
+                type='password'
+                className='mt-2'
+                classNameEye='absolute right-[5px] h-5 w-5 cursor-pointer top-[12px]'
+                placeholder='Password'
+                autoComplete='on'
+              />
+              <div className='mt-3'>
+                <Button
+                  type='submit'
+                  className='flex  w-full items-center justify-center bg-red-500 px-2 py-4 text-sm uppercase text-white hover:bg-red-600'
+                >
+                  Đăng nhập
+                </Button>
+              </div>
+              <div className='mt-8 flex items-center justify-center'>
+                <span className='text-gray-400'>Bạn chưa có tài khoản?</span>
+                <Link className='ml-1 text-red-400' to='/register'>
+                  Đăng ký
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
